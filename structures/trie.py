25a45bc838e7885db36c7b0e8b7956115a5b7eba
Shivam Mehta <shivam.mehta007@gmail.com>
2019-12-24 04:16:44 +0300

    Adding information about trie

--- structures/trie.py
+++ structures/trie.py
@@ -1,1 +1,25 @@
-# Trie
+"""
+Trie
+
+Trie is a datastructure used to store strings for prefix matches or spell check
+In trie every node has a hashmap of key of characters and value of reference to
+another node.
+Time Complexity:
+Insertion of lexicon: O(l*n)
+"""
+
+
+class Trie:
+
+    class _Node:
+
+        def __init__(self, end=True):
+            self.characters = {}
+            self.end = end
+
+    def __init__(self):
+
+        self.root = self._Node()
+
+    def add(self, string):
+        for char in string:
