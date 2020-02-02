07969a40c3358aa2db0eefbac9eb6b66741144b1
Shivam Mehta <shivam.mehta007@gmail.com>
2020-02-03 01:10:38 +0300

    Adding Permutation

--- non_trivial/permutation.py
+++ non_trivial/permutation.py
@@ -1,21 +1,43 @@
+"""
+Permutation
+
+This method is used to generate permutations of a string or an array.
+It will return list of all possible combinations.
+Runtime of this algorithm is O(N!)
+Usage:
+```
+>>>from Permutation import permute
+>>>permute('AB')
+[['A', 'B', 'C'], ['A', 'C', 'B'], ['B', 'A', 'C'], ['B', 'C', 'A'], 
+['C', 'A', 'B'], ['C', 'B', 'A']]
+```
+"""
 class Permutation:
-
+    """
+    Permutation Class
+    """
     @staticmethod
-    def permute(current, fixed_prefix=[], results = []):
-
-        #TODO: permutation of string logic
+    def permute(current, fixed_prefix=[], results=[]):
+        """
+        Permutes string adn arrau"
+        """
         if not current:
             return results.append(fixed_prefix)
 
         if isinstance(current, str):
             current = [x for x in current]
-        for i in range(len(current)):
+        for i, V in enumerate(current):
             next_array = current[:i] + current[i+1:]
-            Permutation.permute(next_array, fixed_prefix + [current[i]], results)
+            Permutation.permute(next_array, fixed_prefix + [V], results)
 
         return results
 
 
 
 if __name__ == '__main__':
-    print(Permutation.permute('ab))
+    from itertools import permutations
+    from random import randint
+    N = randint(2, 10)
+    TESTING_ARRAY = [randint(1, 100) for x in range(N)]
+    assert Permutation.permute(TESTING_ARRAY) == list(map(list, permutations(TESTING_ARRAY)))
+    print("Elementary Test Casses passed")
---
+++ non_trivial/pylint_scores/permutation.py
@@ -0,0 +1,10 @@
+************* Module pythondsa.non_trivial.permutation
+permutation.py:11:68: C0303: Trailing whitespace (trailing-whitespace)
+permutation.py:20:4: W0102: Dangerous default value [] as argument (dangerous-default-value)
+permutation.py:20:4: W0102: Dangerous default value [] as argument (dangerous-default-value)
+permutation.py:29:15: C0103: Variable name "V" doesn't conform to snake_case naming style (invalid-name)
+permutation.py:15:0: R0903: Too few public methods (1/2) (too-few-public-methods)
+
+------------------------------------------------------------------
+Your code has been rated at 7.06/10 (previous run: 7.06/10, +0.00)
+
